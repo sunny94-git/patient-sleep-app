2209fc9ec495b73c5616e74123531ef1ed98b6a3
sunny94-git <rkdsunny@gmail.com>
2026-04-25 09:38:34 +0900

    Add patient prescription page

---
+++ src/app/(patient)/prescription/page.tsx
@@ -0,0 +1,13 @@
+import { requirePatient } from '@/lib/supabase/admin'
+import PrescriptionClient from './PrescriptionClient'
+
+export default async function PrescriptionPage() {
+  const { supabase, patientId } = await requirePatient()
+
+  const [treatmentsRes, examsRes] = await Promise.all([
+    supabase.from('treatment_records').select('*').eq('patient_id', patientId).order('visit_date', { ascending: false }),
+    supabase.from('exam_results').select('*').eq('patient_id', patientId).order('exam_date', { ascending: false }),
+  ])
+
+  return <PrescriptionClient treatments={treatmentsRes.data ?? []} exams={examsRes.data ?? []} />
+}
